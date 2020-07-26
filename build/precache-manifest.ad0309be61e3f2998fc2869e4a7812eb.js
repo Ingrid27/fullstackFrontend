1a170ecda20855531550fbc4e2eaef9e35c9f1e5
Ingrid27 <ingrid.pacheco2015@gmail.com>
2020-07-26 04:03:44 -0300

    26.07.2020

---
+++ build/precache-manifest.ad0309be61e3f2998fc2869e4a7812eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14ea19a764f41a165c51bcf10cc5ce70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d814b9ddb1935a190831",
+    "url": "/static/css/main.5bd2ac59.chunk.css"
+  },
+  {
+    "revision": "7e0f01f2ed95995ad99b",
+    "url": "/static/js/2.6de079b4.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/static/js/2.6de079b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d814b9ddb1935a190831",
+    "url": "/static/js/main.47e4d504.chunk.js"
+  },
+  {
+    "revision": "57d817aca69db69d5789",
+    "url": "/static/js/runtime-main.4ea5ce8e.js"
+  },
+  {
+    "revision": "0e15b9c41472434a8c817482d7cfba10",
+    "url": "/static/media/logo.0e15b9c4.png"
+  }
+]);
